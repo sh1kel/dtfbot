eb93372680f0c25446711391c16f5d7f15116691
Алексей Елагин <a.elagin@corp.youdo.com>
2018-09-25 15:30:50 +0300

    init config file struct

---
+++ configuration.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type Mail struct {
+	Host     string `json:"Host"`
+	ImapPort string `json:"Port"`
+	User     string `json:"Username"`
+	Password string `json:"Password"`
+}
+
+type Db struct {
+	Host     string `json:"Host"`
+	Port     string `json:"Port"`
+	User     string `json:"Username"`
+	Password string `json:"Password"`
+}
+
+type Configuration struct {
+	MailServer Mail `json:"MailAccount"`
+	Postgres   Db   `json:"DbAccount"`
+}
+
+func loadConfig() Configuration {
+	var configData Configuration
+	config, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		fmt.Printf("Can't read config file: %v\n", err)
+		os.Exit(1)
+	}
+	json.Unmarshal(config, &configData)
+	return configData
+}
