9e36317d19876545befaefd07db79dd39440c2c3
Алексей Елагин <a.elagin@corp.youdo.com>
2018-09-25 16:47:51 +0300

    cat get messages

--- imap.go
+++ imap.go
@@ -1,10 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"github.com/emersion/go-imap"
 	"github.com/emersion/go-imap/client"
+	"log"
 )
 
-func connection() {
+func readImapMessages(config *Configuration, messages chan *imap.Message) {
+	imapClient, err := client.DialTLS(config.MailServer.Host+":"+config.MailServer.ImapPort, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	defer imapClient.Logout()
+	err = imapClient.Login(config.MailServer.User, config.MailServer.Password)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mailboxes := make(chan *imap.MailboxInfo, 10)
+	done := make(chan error, 1)
+	go func() {
+		done <- imapClient.List("", "*", mailboxes)
+	}()
+
+	mailBox, err := imapClient.Select("INBOX", false)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Flags for INBOX:", mailBox.Flags)
+
+	from := uint32(1)
+	to := mailBox.Messages
+	fmt.Printf("Number of messages: %d\n", to)
+	seqSet := new(imap.SeqSet)
+	seqSet.AddRange(from, to)
+
+	done = make(chan error, 1)
+	go func() {
+		done <- imapClient.Fetch(seqSet, []imap.FetchItem{imap.FetchEnvelope}, messages)
+	}()
+
+	if err := <-done; err != nil {
+		log.Fatal(err)
+	}
 }
--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/emersion/go-imap"
+)
 
 func main() {
 	var config Configuration
+	mailMessagesChan := make(chan *imap.Message, 100)
 	config = loadConfig()
 	fmt.Printf("%s\n", config)
+	go readImapMessages(&config, mailMessagesChan)
 
+	for msg := range mailMessagesChan {
+		fmt.Printf("# %s\n", msg.Envelope.Subject)
+	}
 }
